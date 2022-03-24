e0d40c314dcda190e6ef5005c677f70bad2c0810
peterpaw <pb.pawelczyk@gmail.com>
2022-03-24 10:45:19 +0100

    Add sidebar to layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,12 +1,17 @@
+import { useState } from "react"
 import Footer from "./Footer"
 import Navbar from "./Navbar"
+import Sidebar from "./Sidebar"
 
 const Layout: React.FC = ({ children }) => {
+  const [showSidebar, setShowSidebar] = useState(true)
+
   return (
     <>
       <Navbar />
       {children}
       <Footer />
+      <Sidebar showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
     </>
   )
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,32 +1,27 @@
-import { useEffect } from "react"
 import Image from "next/image"
 import Link from "next/link"
 import { useMediaQuery } from "@mantine/hooks"
+import { MenuIcon } from "@heroicons/react/outline"
 
 import { navigation } from "data/navigation"
 
 import rwwLogo from "../public/rww-logo.png"
 
 const Navbar = () => {
-  const isMobile = useMediaQuery("(max-width: 500px)")
-
-  useEffect(() => {
-    console.log(isMobile)
-  }, [isMobile])
+  const isDesktop = useMediaQuery("(min-width: 768px)")
 
   return (
     <div className="sticky top-0 z-20 border-b bg-opacity-80 bg-gray-100 backdrop-filter backdrop-blur-lg shadow-lg shadow-slate-600/10">
       <div className="max-w-7xl mx-auto px-4 sm:px-6">
-        <div className="relative py-2 md:space-x-10 md:justify-between justify-center flex items-center flex-wrap content-center">
+        <div className="relative py-2 md:space-x-10 justify-between flex items-center flex-wrap content-center">
           <div className="justify-center flex">
             <Link href="/">
-              <a className="leading-none">
+              <a className="leading-none relative h-10 w-10 md:h-12 md:w-12">
                 <span className="sr-only">badminton-walldorf.de</span>
                 <Image
                   src={rwwLogo}
                   alt="Rot-Weiss Walldorf Logo"
-                  width={50}
-                  height={50}
+                  layout="fill"
                 />
               </a>
             </Link>
@@ -44,6 +39,7 @@ const Navbar = () => {
               ))}
             </div>
           </nav>
+          {!isDesktop && <MenuIcon className="w-8 h-8" />}
         </div>
       </div>
     </div>
