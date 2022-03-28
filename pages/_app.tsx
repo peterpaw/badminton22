5495097e121fd29c233e68c53e331a447fb91f86
peterpaw <pb.pawelczyk@gmail.com>
2022-03-28 18:25:14 +0200

    Add dark mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,12 +9,16 @@ import {
   Header,
   MediaQuery,
   Navbar,
-  Text,
   useMantineTheme,
   createStyles,
   Anchor,
   Image,
+  ColorSchemeProvider,
+  ColorScheme,
+  ActionIcon,
 } from "@mantine/core"
+import { useLocalStorage } from "@mantine/hooks"
+import { SunIcon, MoonIcon } from "@heroicons/react/outline"
 
 import { navigation } from "data/navigation"
 // import Layout from "@components/Layout"
@@ -39,6 +43,14 @@ function MyApp({ Component, pageProps }: AppProps) {
   const [opened, setOpened] = useState(false)
   const theme = useMantineTheme()
   const { classes } = useStyles()
+  const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+    key: "mantine-color-scheme",
+    defaultValue: "light",
+  })
+  const dark = colorScheme === "dark"
+
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"))
 
   return (
     <>
@@ -49,96 +61,122 @@ function MyApp({ Component, pageProps }: AppProps) {
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
       </Head>
-      <MantineProvider
-        withGlobalStyles
-        withNormalizeCSS
-        theme={{
-          /** Put your mantine theme override here */
-          colorScheme: "light",
-        }}
+      <ColorSchemeProvider
+        colorScheme={colorScheme}
+        toggleColorScheme={toggleColorScheme}
       >
-        {/* <Layout> */}
-        <AppShell
-          // navbarOffsetBreakpoint controls when navbar should no longer be offset with padding-left
-          navbarOffsetBreakpoint="sm"
-          // fixed prop on AppShell will be automatically added to Header and Navbar
-          fixed
-          header={
-            <Header height={55} p="md">
-              {/* Handle other responsive styles with MediaQuery component or createStyles function */}
-              <div
-                style={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "space-between",
-                  height: "100%",
-                }}
-              >
+        <MantineProvider
+          withGlobalStyles
+          withNormalizeCSS
+          theme={{
+            colorScheme,
+          }}
+        >
+          {/* <Layout> */}
+          <AppShell
+            padding={0}
+            // navbarOffsetBreakpoint controls when navbar should no longer be offset with padding-left
+            // navbarOffsetBreakpoint="xs"
+            // fixed prop on AppShell will be automatically added to Header and Navbar
+            fixed
+            header={
+              <Header height={55} p="md">
+                {/* Handle other responsive styles with MediaQuery component or createStyles function */}
                 <div
                   style={{
-                    width: 50,
-                    padding: "0.125rem",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                    height: "100%",
                   }}
                 >
-                  <Image
-                    radius="md"
-                    src="/rww-logo.png"
-                    alt="Rot-Weiß Walldorf Logo"
-                  />
-                </div>
-                <MediaQuery largerThan="sm" styles={{ display: "none" }}>
-                  <Burger
-                    opened={opened}
-                    onClick={() => setOpened((o) => !o)}
-                    size="sm"
-                    color={theme.colors.gray[6]}
-                    mr="xl"
-                  />
-                </MediaQuery>
-                <div className={classes.links}>
-                  <div className="flex gap-1">
-                    {navigation.map((navLink) => (
-                      <Anchor
-                        component={Link}
-                        href={navLink.href}
-                        key={navLink.name}
-                      >
-                        <a className="text-sm font-medium text-gray-700">
-                          {navLink.name}
-                        </a>
-                      </Anchor>
-                    ))}
+                  <div
+                    style={{
+                      width: 50,
+                      padding: "0.25rem",
+                    }}
+                  >
+                    <Anchor component={Link} href="/">
+                      <a>
+                        <Image
+                          radius="md"
+                          src="/rww-logo.png"
+                          alt="Rot-Weiß Walldorf Logo"
+                        />
+                      </a>
+                    </Anchor>
+                  </div>
+                  <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+                    <Burger
+                      opened={opened}
+                      onClick={() => setOpened((o) => !o)}
+                      size="md"
+                      color={theme.colors.gray[8]}
+                    />
+                  </MediaQuery>
+                  <div className="flex gap-4 items-center">
+                    <div className={classes.links}>
+                      <div className="flex gap-4">
+                        {navigation.map((navLink) => (
+                          <Anchor
+                            component={Link}
+                            href={navLink.href}
+                            key={navLink.name}
+                          >
+                            <a className="text-sm font-medium text-gray-700 tracking-wider">
+                              {navLink.name}
+                            </a>
+                          </Anchor>
+                        ))}
+                      </div>
+                    </div>
+                    <ActionIcon
+                      variant="outline"
+                      color={dark ? "yellow" : "blue"}
+                      onClick={() => toggleColorScheme()}
+                      title="Toggle color scheme"
+                    >
+                      {dark ? (
+                        <SunIcon className="w-8 h-8" />
+                      ) : (
+                        <MoonIcon className="w-8 h-8" />
+                      )}
+                    </ActionIcon>
                   </div>
                 </div>
-              </div>
-            </Header>
-          }
-          navbar={
-            <Navbar
-              className={classes.navbar}
-              p="md"
-              // Breakpoint at which navbar will be hidden if hidden prop is true
-              hiddenBreakpoint="sm"
-              // Hides navbar when viewport size is less than value specified in hiddenBreakpoint
-              hidden={!opened}
-              // when viewport size is less than theme.breakpoints.sm navbar width is 100%
-              // viewport size > theme.breakpoints.sm – width is 300px
-              // viewport size > theme.breakpoints.lg – width is 400px
-              width={{ base: "100%", sm: 0 }}
-            >
-              {navigation.map((navLink) => (
-                <Anchor component={Link} href={navLink.href} key={navLink.name}>
-                  <a onClick={() => setOpened(false)}>{navLink.name}</a>
-                </Anchor>
-              ))}
-            </Navbar>
-          }
-        >
-          <Component {...pageProps} />
-        </AppShell>
+              </Header>
+            }
+            navbar={
+              <Navbar
+                className={classes.navbar}
+                p="md"
+                // Breakpoint at which navbar will be hidden if hidden prop is true
+                hiddenBreakpoint="sm"
+                // Hides navbar when viewport size is less than value specified in hiddenBreakpoint
+                hidden={!opened}
+                // when viewport size is less than theme.breakpoints.sm navbar width is 100%
+                // viewport size > theme.breakpoints.sm – width is 300px
+                // viewport size > theme.breakpoints.lg – width is 400px
+                width={{ base: "100%", sm: 0 }}
+              >
+                {navigation.map((navLink) => (
+                  <Anchor
+                    component={Link}
+                    href={navLink.href}
+                    key={navLink.name}
+                  >
+                    <a onClick={() => setOpened(false)}>{navLink.name}</a>
+                  </Anchor>
+                ))}
+              </Navbar>
+            }
+          >
+            <Component {...pageProps} />
+          </AppShell>
 
-        {/* </Layout> */}
-      </MantineProvider>
+          {/* </Layout> */}
+        </MantineProvider>
+      </ColorSchemeProvider>
     </>
   )
 }
