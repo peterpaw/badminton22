fcf0c478de72098d5e2c97bdd32affeb944828fb
peterpaw <pb.pawelczyk@gmail.com>
2022-03-29 21:11:00 +0200

    Add testing page

---
+++ pages/landing.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image"
+
+const LandingPage = () => {
+  return (
+    <div className="relative w-full h-full">
+      <div className="h-full w-full absolute flex justify-center items-center">
+        <Image
+          src="/landing-gradient.jpg"
+          alt="Landing Photo"
+          layout="fill"
+          objectFit="cover"
+        />
+        <h1 className="absolute">Landing Page</h1>
+      </div>
+    </div>
+  )
+}
+
+export default LandingPage
