8fa6bf2cd310a206b002919764f045dd2afa2aac
perterpaw <pb.pawelczyk@gmail.com>
2022-04-03 17:21:58 +0200

    Add photo to author pages

--- pages/autoren/[slug].tsx
+++ pages/autoren/[slug].tsx
@@ -3,10 +3,13 @@ import Head from "next/head"
 import { gql, GraphQLClient } from "graphql-request"
 
 import { PostDetailsType } from "types"
-import { Paper, Text } from "@mantine/core"
+import { Paper, Text, useMantineColorScheme } from "@mantine/core"
 import { format } from "date-fns"
 import { de } from "date-fns/locale"
 import Link from "next/link"
+import Image from "next/image"
+import { cn } from "@utils/cn"
+import { useState } from "react"
 
 interface PageProps {
   data: {
@@ -28,9 +31,34 @@ interface PageProps {
 
 const client = new GraphQLClient(process.env.NEXT_PUBLIC_GRAPHCMS_URL as string)
 
+function BlurImage({ src, authorName }: { src: string; authorName: string }) {
+  const [isLoading, setIsLoading] = useState(true)
+
+  return (
+    <div className="aspect-w-1 w-16 h-16 overflow-hidden rounded-full mx-auto mb-16">
+      <Image
+        src={src}
+        alt={authorName}
+        layout="fill"
+        objectFit="cover"
+        className={cn(
+          "hover:opacity-75 duration-700 ease-in-out",
+          isLoading
+            ? "grayscale blur-2xl scale-110"
+            : "grayscale-0 blur-0 scale-100"
+        )}
+        onLoadingComplete={() => setIsLoading(false)}
+      />
+    </div>
+  )
+}
+
 const AuthorPage: NextPage<PageProps> = ({ data }) => {
   const { edges } = data?.postsConnection
   const { author } = data
+
+  const { colorScheme } = useMantineColorScheme()
+  const dark = colorScheme === "dark"
 
   return (
     <>
@@ -41,10 +69,20 @@ const AuthorPage: NextPage<PageProps> = ({ data }) => {
       <main className="py-16 mx-auto max-w-3xl">
         <Text
           component="h1"
-          className="text-xl md:text-2xl font-black text-center mb-16"
+          className="text-xl md:text-2xl font-black text-center mb-0"
         >
-          {edges.length} Beiträge von {author.name}
+          {author.name}
         </Text>
+        <Text
+          component="h3"
+          sx={(theme) => ({
+            color: dark ? theme.colors.gray[6] : theme.colors.gray[5],
+          })}
+          className="mb-4"
+        >
+          {edges.length} Beiträge
+        </Text>
+        <BlurImage src={author.foto.url} authorName={author.name} />
         {edges.map(({ node }) => {
           const date = format(new Date(node.postPublishDate), "dd. MMMM yyyy", {
             locale: de,
@@ -58,6 +96,9 @@ const AuthorPage: NextPage<PageProps> = ({ data }) => {
                 p="md"
                 my="sm"
                 sx={(theme) => ({
+                  backgroundColor: dark
+                    ? theme.colors.dark[4]
+                    : theme.colors.gray[0],
                   "&:hover": { color: theme.colors.red[5] },
                 })}
                 className="duration-500 ease-in-out"
