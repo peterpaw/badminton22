4fbc13623c1f157fb987a3918eb0b2f9b2e63e2e
peterpaw <pb.pawelczyk@gmail.com>
2022-05-01 11:08:59 +0200

    Fix animation and links on teams page

--- components/TeamListItem.tsx
+++ components/TeamListItem.tsx
@@ -1,10 +1,11 @@
 import { Container, Text, Title, useMantineColorScheme } from "@mantine/core"
 import { useHover } from "@mantine/hooks"
-import { cn } from "@utils/cn"
 import { motion } from "framer-motion"
 import Link from "next/link"
-import { TeamListTypes } from "types"
+
 import BlurImg from "./BlurImg"
+
+import { TeamListTypes } from "types"
 
 const TeamListItem = ({
   team,
@@ -18,11 +19,11 @@ const TeamListItem = ({
 
   const even = index % 2 === 0
 
-  const { hovered, ref } = useHover()
+  const { hovered: hovered1, ref: ref1 } = useHover()
+  const { hovered: hovered2, ref: ref2 } = useHover()
 
   return (
     <Container
-      ref={ref}
       fluid
       sx={(theme) => ({
         backgroundColor: dark
@@ -35,88 +36,91 @@ const TeamListItem = ({
       })}
       className="pb-16 md:pt-8"
     >
-      <Link href={`/mannschaften/${team.slug}`}>
-        <a>
-          <Container className="grid grid-cols-12">
-            <motion.div
-              initial={{ x: even ? 200 : -200, opacity: 0 }}
-              whileInView={{
-                opacity: 1,
-                x: 0,
-                transition: {
-                  duration: 1.2,
-                  type: "spring",
-                  bounce: 0.4,
-                },
-              }}
-              viewport={{ margin: "0px", once: true }}
-              className="col-span-12 md:col-span-8 col-start-1 md:col-start-3"
-            >
+      <Container className="grid grid-cols-12">
+        <motion.div
+          initial={{ x: even ? 200 : -200, opacity: 0 }}
+          whileInView={{
+            opacity: 1,
+            x: 0,
+            transition: {
+              duration: 1.2,
+              type: "spring",
+              bounce: 0.4,
+            },
+          }}
+          viewport={{ margin: "0px", once: true }}
+          className="col-span-12 md:col-span-8 col-start-1 md:col-start-3"
+        >
+          <Link href={`/mannschaften/${team.slug}`}>
+            <a>
               <Title
+                ref={ref1}
                 order={2}
-                sx={(theme) => ({ backgroundColor: theme.colors.red[5] })}
-                className="text-base md:text-2xl mb-2 px-0 md:px-32 py-1 text-white"
+                sx={(theme) => ({
+                  backgroundColor:
+                    hovered1 || hovered2
+                      ? theme.colors.red[2]
+                      : theme.colors.red[5],
+                })}
+                className="text-base md:text-2xl mb-2 px-0 md:px-32 py-1 text-white duration-300"
               >
                 {team.mannschaft}
               </Title>
-            </motion.div>
-            <motion.div
-              initial={{ x: even ? -200 : 200, opacity: 0 }}
-              whileInView={{
-                opacity: 1,
-                x: 0,
-                transition: {
-                  duration: 1.2,
-                  type: "spring",
-                  bounce: 0.4,
-                  delay: 0.4,
-                },
-              }}
-              viewport={{ margin: "0px", once: true }}
-              className="col-span-12"
-            >
-              <Text
-                component="p"
-                sx={(theme) => ({
-                  color: hovered
-                    ? theme.colors.red[5]
-                    : dark
-                    ? theme.colors.dark[0]
-                    : theme.colors.gray[7],
-                })}
-                className="text-base md:text-xl font-bold italic text-center duration-300"
-              >
-                {team.liga}
-              </Text>
-            </motion.div>
-            <motion.div
-              initial={{ x: even ? 200 : -200, opacity: 0 }}
-              whileInView={{
-                opacity: 1,
-                x: 0,
-                transition: {
-                  duration: 1.2,
-                  type: "spring",
-                  bounce: 0.4,
-                  delay: 0.8,
-                },
-              }}
-              viewport={{ margin: "0px", once: true }}
-              className={cn(
-                "aspect-w-16 aspect-h-9 w-full col-start-1 md:col-start-4 col-span-12 md:col-span-6 bg-gray-200",
-                hovered ? "opacity-75 duration-300 ease-in-out" : ""
-              )}
-            >
+            </a>
+          </Link>
+        </motion.div>
+        <motion.div
+          initial={{ x: even ? -200 : 200, opacity: 0 }}
+          whileInView={{
+            opacity: 1,
+            x: 0,
+            transition: {
+              duration: 1.2,
+              type: "spring",
+              bounce: 0.4,
+              delay: 0.4,
+            },
+          }}
+          viewport={{ margin: "0px", once: true }}
+          className="col-span-12"
+        >
+          <Text
+            component="p"
+            sx={(theme) => ({
+              color: dark ? theme.colors.dark[0] : theme.colors.gray[7],
+            })}
+            className="text-base md:text-xl font-bold italic text-center duration-300"
+          >
+            {team.liga}
+          </Text>
+        </motion.div>
+        <motion.div
+          ref={ref2}
+          initial={{ x: even ? 200 : -200, opacity: 0 }}
+          whileInView={{
+            opacity: 1,
+            x: 0,
+            transition: {
+              duration: 1.2,
+              type: "spring",
+              bounce: 0.4,
+              delay: 0.8,
+            },
+          }}
+          viewport={{ margin: "0px", once: true }}
+          className="aspect-w-16 aspect-h-9 w-full col-start-1 md:col-start-4 col-span-12 md:col-span-6"
+        >
+          <Link href={`/mannschaften/${team.slug}`}>
+            <a>
               <BlurImg
                 src={team.teamPhoto.url}
                 alt={`Mannschaftsfoto ${team.mannschaft}`}
                 priority="true"
-                hovered={hovered}
               />
-            </motion.div>
-          </Container>
-        </a>
-      </Link>
+            </a>
+          </Link>
+        </motion.div>
+      </Container>
     </Container>
   )
 }
