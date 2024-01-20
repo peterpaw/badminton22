598f75a22f4f372d7a121d4d6ba94f9f66430807
Peter Pawelczyk <pb.pawelczyk@gmail.com>
2024-01-20 11:10:11 +0100

    Make table responsive

--- components/Table.tsx
+++ components/Table.tsx
@@ -13,27 +13,27 @@ export default function LeagueTable({ table }: { table: Table }) {
   const rows = table.map((element) => (
     <tr key={element.id}>
       <td>{element.rang}</td>
-      <td>{element.mannschaft}</td>
-      <td>{element.s}</td>
-      <td>{element.u}</td>
-      <td>{element.n}</td>
+      <td className="text-left">{element.mannschaft}</td>
+      <td className="hidden md:table-cell">{element.s}</td>
+      <td className="hidden md:table-cell">{element.u}</td>
+      <td className="hidden md:table-cell">{element.n}</td>
       <td>{element.begegnungen}</td>
       <td>{element.punkte}</td>
     </tr>
   ));
 
   return (
-    <Container>
+    <Container pl={0} pr={0}>
       <MTable withColumnBorders>
         <thead>
           <tr>
-            <th style={{ textAlign: "center" }}>Rang</th>
-            <th style={{ textAlign: "center" }}>Mannschaft</th>
-            <th style={{ textAlign: "center" }}>S</th>
-            <th style={{ textAlign: "center" }}>U</th>
-            <th style={{ textAlign: "center" }}>N</th>
-            <th style={{ textAlign: "center" }}>Spiele</th>
-            <th style={{ textAlign: "center" }}>Punkte</th>
+            <th style={{ textAlign: "center" }}></th>
+            <th>Mannschaft</th>
+            <th className="hidden md:table-cell" style={{ textAlign: "center" }}>S</th>
+            <th className="hidden md:table-cell" style={{ textAlign: "center" }}>U</th>
+            <th className="hidden md:table-cell" style={{ textAlign: "center" }}>N</th>
+            <th style={{ textAlign: "center" }}>Sp<span className="hidden md:inline-block">iele</span></th>
+            <th style={{ textAlign: "center" }}>P<span className="hidden md:inline-block">un</span>kt<span className="hidden md:inline-block">e</span></th>
           </tr>
         </thead>
         <tbody>{rows}</tbody>
@@ -43,6 +43,5 @@ export default function LeagueTable({ table }: { table: Table }) {
         className="text-xs font-semibold leading-none text-center mt-8"
       >Zuletzt aktualisiert am {lastUpdated}</Text>
     </Container>
-
   );
 }
